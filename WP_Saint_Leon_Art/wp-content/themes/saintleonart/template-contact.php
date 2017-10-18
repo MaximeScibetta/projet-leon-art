ff83a90d9cc18165984bb59d9dbbdfe6cadfe1e2
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-10-18 13:54:04 +0200

    Fix contact page teacher's commentary

--- WP_Saint_Leon_Art/wp-content/themes/saintleonart/template-contact.php
+++ WP_Saint_Leon_Art/wp-content/themes/saintleonart/template-contact.php
@@ -12,20 +12,8 @@ get_header();
         <p>
             Vous pouvez nous contacter via ce formulaire de contact pour n'importe quelle demande. Du partenariat en passant par la simple question nous sommes à votre entière disposition !
         </p>
-       <div class="content">
-        <svg version="1.1" id="logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 200 200" style="enable-background:new 0 0 200 200;">
-            <style type="text/css">
-                .st0{fill:#D94636;}
-                .st1{fill:#FFCC79;}
-                .st2{fill:#7F174A;}
-            </style>
-            <g>
-                <path class="st0" d="M178.2,99.8c0,42.8-34.7,77.4-77.4,77.4c-42.8,0-77.4-34.7-77.4-77.4c0-42.8,34.7-77.4,77.4-77.4
-                    C143.5,22.4,178.2,57,178.2,99.8"/>
-            </g>
-            <rect x="60.3" y="10.8" class="st1" width="80.9" height="177.9"/>
-            <polygon class="st2" points="187.9,106.3 13.6,2.9 13.6,92.8 13.6,93.2 187.9,196.7 187.9,106.7 "/>
-        </svg>
+        <div class="content">
+            <span>SaintLeon'Art</span>
             <div class="info">
                 <div>
                     <svg version="1.1" id="location" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 54.757 54.757" style="enable-background:new 0 0 54.757 54.757;">
