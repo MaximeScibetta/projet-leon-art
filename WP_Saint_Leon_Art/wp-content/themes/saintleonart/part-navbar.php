98c8887e4da6aedf4da961897af36763ab2e4c55
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-11-10 11:24:28 +0100

    Add menu position on my active item menu

--- WP_Saint_Leon_Art/wp-content/themes/saintleonart/part-navbar.php
+++ WP_Saint_Leon_Art/wp-content/themes/saintleonart/part-navbar.php
@@ -3,7 +3,7 @@
     <ul class="menu">
         <li class="menu__item sla">SaintLeon'Art</li>
     <?php foreach(dw_get_nav_items('header') as $item):?>
-        <li class="menu__item">
+        <li class="menu__item <?php if( $item->link === get_permalink() ) :?>active<?php endif; ?>">
             <a href="<?php echo $item->link;?>"> <?php echo $item->label;?> </a>
         </li>  
     <?php endforeach;?>
