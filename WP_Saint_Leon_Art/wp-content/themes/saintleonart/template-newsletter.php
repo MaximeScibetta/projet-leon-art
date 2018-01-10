cc0714893882019df37897670ba72ba1d4dd312a
Maxime Scibetta <maxime.scibetta@outlook.com>
2018-01-10 19:38:02 +0100

    Make newsletterValidaiton

---
+++ WP_Saint_Leon_Art/wp-content/themes/saintleonart/template-newsletter.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Page Newsletter    
+ */
+get_header();
+?>
+<section class="introduction litle" style="background-image: url(<?= dw_asset('images/contact.jpg'); ?>);">
+    <h2 id="branding">newsletter <span>SaintLeon'Art</span></h2>
+</section>
+<section class="newsletterValidation">
+    <div class="success-message">
+        <svg viewBox="0 0 76 76" class="success-message__icon icon-checkmark">
+            <circle cx="38" cy="38" r="36"/>
+            <path fill="none" stroke="#FFFFFF" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M17.7,40.9l10.9,10.9l28.7-28.7"/>
+        </svg>
+        <h1 class="success-message__title">Inscription à la newsletter validé</h1>
+        <div class="success-message__content">
+            <p>Vous allez recevoir un email de confirmation d'ici 15 minutes.</p>
+            <p>Merci de votre confiance & à bientôt !</p>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
